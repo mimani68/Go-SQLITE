d6db73b339029c9f0c6ce62095b9e4d960cc8e23
mahdi <imani@iran.ir>
2021-11-15 18:09:52 +0330

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kelseyhightower/echo
 
 go 1.12
+
+require github.com/mattn/go-sqlite3 v1.14.9 // indirect
--- main.go
+++ main.go
@@ -1,69 +1,105 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
-	"net"
 	"net/http"
-	"os"
+
+	"github.com/google/uuid"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Message struct {
-	Message string `json:"message"`
+func simpleService() interface{} {
+	resp, _ := http.Get("https://run.mocky.io/v3/6376ab35-cf8e-4cec-9aaf-f474fcb0f960")
+	body, _ := io.ReadAll(resp.Body)
+	// fmt.Printf("%s", body)
+	return fmt.Sprintf("%s", body)
+}
+
+var db *sql.DB
+
+func createConnection(dbName string) {
+	// os.Remove("./app.db")
+
+	var err error
+	db, err = sql.Open("sqlite3", fmt.Sprintf("./%s.db", dbName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//
+	// defer db.Close()
+}
+
+func createTable(name string) {
+	sqlStmt := `
+	create table ` + name + ` (id integer not null primary key, data text);
+	`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+}
+
+func storeToDb(tName string, data string) {
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into " + tName + "(id, data) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	// for i := 0; i < 100; i++ {
+	// 	_, err = stmt.Exec(i, fmt.Sprintf("text number %03d", i))
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
+	_, err = stmt.Exec(uuid.New().String(), data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tx.Commit()
+}
+
+func readOperationFromDb(tName string) interface{} {
+	var id int
+	var name string
+	rows, err := db.Query("select id, data from " + tName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return struct {
+		id   int
+		data string
+	}{
+		id:   id,
+		data: name,
+	}
 }
 
 func main() {
-	log.Println("Starting echo service ...")
-
-	httpListenPort := os.Getenv("PORT")
-	if httpListenPort == "" {
-		httpListenPort = "8080"
-	}
-
-	hostPort := net.JoinHostPort("0.0.0.0", httpListenPort)
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		return
-	})
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "", 500)
-			return
-		}
-		defer r.Body.Close()
-
-		var message Message
-		err = json.Unmarshal(data, &message)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "", 500)
-			return
-		}
-
-		responseData, err := json.MarshalIndent(message, "", "  ")
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "", 500)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintf(w, string(responseData))
-	})
-
-	s := &http.Server{
-		Addr:    hostPort,
-		Handler: mux,
-	}
-
-	if err := s.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	a := simpleService()
+	createConnection("app")
+	createTable("userSession")
+	storeToDb("userSession", a.(string))
+	fmt.Println(readOperationFromDb("userSession"))
+	defer db.Close()
 }
